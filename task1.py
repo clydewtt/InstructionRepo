8e6515b01c8e817f5364e1de8bb11f731b5a27ee
Clyde T <84462113+clydewtt@users.noreply.github.com>
2024-01-15 21:38:23 -0500

    Create task1.py

---
+++ task1.py
@@ -0,0 +1,8 @@
+# You have reached the test python file in today's workshop
+
+# Task: Fix this block of code
+
+def get_number_5():
+  return 4
+
+assert get_number_5() == 5, "The function should return 5"
