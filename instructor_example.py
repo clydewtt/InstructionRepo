52caa9230f28526e3acff1a99b6a2e3ea773f399
ClydeWTT <tandjong.c@gmail.com>
2024-01-16 02:18:14 -0500

    test fix instructor example

--- instructor_example.py
+++ instructor_example.py
@@ -1,7 +1,7 @@
 # This function should return the best data structure of all time!
 
 def get_greatest_data_structure():
-  return "Stack"
+  return "Hash Table"
 
 assert get_greatest_data_structure() == "Hash Table", "A stack is not the greatest data structure of all time"
 
