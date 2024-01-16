904951881443432624d317d1a4dad6180ecfb713
Clyde T <84462113+clydewtt@users.noreply.github.com>
2024-01-16 02:06:24 -0500

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,8 +1,6 @@
-# You have reached the test python file in today's workshop
-
-# Task: Fix this block of code
+# This function should return the number 5
 
 def get_number_5():
-  return 5
+  return 4
 
 assert get_number_5() == 5, "The function should return 5"
