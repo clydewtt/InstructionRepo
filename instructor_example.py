69b32d5ee5b880d24d03b7e5936fb99bb8ede0e3
Clyde T <84462113+clydewtt@users.noreply.github.com>
2024-01-16 02:13:07 -0500

    Create instructor_example.py

---
+++ instructor_example.py
@@ -0,0 +1,8 @@
+# This function should return the best data structure of all time!
+
+def get_greatest_data_structure():
+  return "Stack"
+
+assert get_greatest_data_structure() == "Hash Table"
+
+print("Test Passed!")
