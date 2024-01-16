7b3bf931b93a44424b8344031698b72fb8112835
Clyde T <84462113+clydewtt@users.noreply.github.com>
2024-01-16 13:09:36 -0500

    Create task9.py

---
+++ task9.py
@@ -0,0 +1,8 @@
+# This function should return whether or not the argument is an even number
+
+def is_even(num):
+    return num % 2 != 0
+
+assert is_even(5525) == False, "5525 is not even, the function should return False"
+
+print("Test passed!")
