2195b7379b8dd8b2a0ead510fe73c5894c91bac9
michaeladeleke1 <miade10@morgan.edu>
2024-01-18 14:53:51 -0500

    Changed return len(lst) == 4 to 5

--- task5.py
+++ task5.py
@@ -1,7 +1,7 @@
 # This function should return True if the argument's list size is 5
 
 def is_list_length_5(lst):
-    return len(lst) == 4
+    return len(lst) == 5
 
 assert is_list_length_5([1,2,3,4,5]) == True, "The length of the list is 5, function should return True"
 
