f1997eed616fda80e37d0acfe27cc7c784f5573a
Andrew Kelly <drewkelly0320@gmail.com>
2024-01-18 15:02:11 -0500

    fixed task 6

--- task6.py
+++ task6.py
@@ -1,7 +1,7 @@
 # This function should return whether or not the argument is an empty string
 
 def is_string_empty(string):
-    return string == "sds"
+    return string == ""
 
 assert is_string_empty("") == True, "The string is empty, function should return True"
 
