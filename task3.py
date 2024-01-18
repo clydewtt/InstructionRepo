2341ad6a80f3c16f8e6b972ee5c8d4a9b6e83796
kingcoder07 <127437568+kingcoder07@users.noreply.github.com>
2024-01-18 15:07:54 -0500

    Update task3.py

--- task3.py
+++ task3.py
@@ -1,7 +1,7 @@
-# This function should check if the name argument is Bobby
+This function should check if the name argument is Bobby
 
 def is_name_bobby(name):
-    return name != "Bobby"
+    return name == "Bobby"
 
 assert is_name_bobby("Alice") == False, "Alice is not Bobby, should return False"
 
